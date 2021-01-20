0552c669191dacc9fe46b688270d33b07984e8fe
harrison.tom <harrison.tom@kakaocorp.com>
2021-01-20 21:19:06 +0900

    장바구니 구조체 구현

---
+++ EmoticonApp/EmoticonApp/Model/cart.swift
@@ -0,0 +1,60 @@
+//
+//  cart.swift
+//  EmoticonApp
+//
+//  Created by 이준형 on 2021/01/19.
+//  Copyright © 2021 이준형. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefault {
+    
+    static let db = UserDefaults.standard
+
+    static func getData() -> Data? {
+        return db.data(forKey: "histories") ?? nil
+    }
+
+    static func addData(_ dataArray: [History]) {
+        if let data = try? PropertyListEncoder().encode(dataArray){
+            UserDefault.db.set(data, forKey: "histories")
+        }
+    }
+}
+
+struct Cart {
+    static var myEmoticons: [History] = {
+        var array: [History] = []
+        if let data = UserDefault.getData() {
+            let dataArray = try! PropertyListDecoder().decode([History].self, from: data)
+            array = dataArray
+        }
+        return array
+    }()
+
+     static var count: Int {
+        self.myEmoticons.count
+    }
+
+    static func buyEmoticon(title: String, date: String) {
+        self.myEmoticons.append(History(title: title, date: date))
+        UserDefault.addData(self.myEmoticons)
+    }
+
+    static func remove(index: Int) {
+        self.myEmoticons.remove(at: index)
+    }
+    static func removeAll() {
+        self.myEmoticons.removeAll()
+    }
+    
+    static subscript(index: Int) -> History {
+        return self.myEmoticons[index]
+    }
+}
+
+struct History: Codable {
+    let title: String
+    let date: String
+}
